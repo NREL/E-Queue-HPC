2fddd958aafbcf3a875be3fa6651bc0ac83d66f6
ctripp <charles.tripp@nrel.gov>
2022-01-18 15:54:20 -0700

    + updated init files

--- jobqueue/__init__.py
+++ jobqueue/__init__.py
@@ -1,2 +1,4 @@
 from .job_queue import JobQueue
 from .job import Job
+from .connect import load_credentials, close_pools, acquire_pooled_connection, release_pooled_connection, connect
+from .cursor_manager import CursorManager
