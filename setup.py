4c668b9bd13345d136b5473bbccde4c6384501dd
ctripp <charles.tripp@nrel.gov>
2023-01-13 14:15:47 -0700

    + update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,12 @@ from setuptools import setup
 setup(
     name="jobqueue",
     version="0.0.1",
-    install_requires=["pytest", "pandas", "psycopg>=3"],  # list dependencies
+    install_requires=[
+        "pytest",
+        "pandas",
+        "psycopg>=3",
+        "psycopg_pool>=3",
+    ],  # list dependencies
     entry_points={
         "console_scripts": [
             "jq=jobqueue.main:main",
